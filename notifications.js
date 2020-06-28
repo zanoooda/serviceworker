96dd1b57d3b0e5e3b75daa6c52a5770f49b0eade
zanooda <zanooda@ya.ru>
2020-06-28 14:31:51 +0300

    add onMessage and onTokenRefresh listeners

--- notifications.js
+++ notifications.js
@@ -6,6 +6,41 @@ firebase.initializeApp({
 
 var messaging = firebase.messaging();
 
+// handle catch the notification on current page
+messaging.onMessage(function(payload) {
+    console.log('Message received', payload);
+
+    // register fake ServiceWorker for show notification on mobile devices
+    navigator.serviceWorker.register('/serviceworker/firebase-messaging-sw.js');
+    Notification.requestPermission(function(permission) {
+        if (permission === 'granted') {
+            navigator.serviceWorker.ready.then(function(registration) {
+                // Copy data object to get parameters in the click handler
+                payload.data.data = JSON.parse(JSON.stringify(payload.data));
+                registration.showNotification(payload.data.title, payload.data);
+
+                alert(JSON.stringify(payload))
+            }).catch(function(error) {
+                // registration failed :(
+                alert('ServiceWorker registration failed', error);
+            });
+        }
+    });
+});
+
+// Callback fired if Instance ID token is updated.
+messaging.onTokenRefresh(function() {
+    messaging.getToken()
+        .then(function(refreshedToken) {
+            console.log('Token refreshed');
+
+            document.getElementById('current-token').innerHTML = refreshedToken;
+        })
+        .catch(function(error) {
+            alert('Unable to retrieve refreshed token', error);
+        });
+});
+
 getToken()
 
 function getToken() {
